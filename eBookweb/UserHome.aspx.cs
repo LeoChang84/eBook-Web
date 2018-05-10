a517b7a88d97032481dd8f7414f7c9c5a3e68cef
LeoChang84 <leochang9384@gmail.com>
2018-05-11 00:48:13 +0800

    add UserHomePage

---
+++ eBookweb/UserHome.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace eBookweb
+{
+    public partial class UserHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["USERNAME"] != null)
+            {
+                lbUserHomesuccess.Text = "Login Success, Welcome "+Session["USERNAME"].ToString()+"";
+            }
+            else
+            {
+                Response.Redirect("~/SignIn.aspx");
+            }
+        }
+
+        protected void btUserHomeSignOut_Click(object sender, EventArgs e)
+        {
+            Session["USERNAME"] = null;
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+}
